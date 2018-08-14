a96f7d299014bca9bb4be5fd1c3521be835e03c4
rochellesagh <rsrochelli@gmail.com>
2018-08-14 15:20:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+
+    "My name is Rochelle."
 end
+
+
+  get '/hometown' do 
+
+    "My hometown is Great Neck,NY."
+end
+
+
+
+ get '/favorite-song' do 
+
+    "My favorite song is Dani California by The Red Hot Chili Peppers."
+end
+end 
+
